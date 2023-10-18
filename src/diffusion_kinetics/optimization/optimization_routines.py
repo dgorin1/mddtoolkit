8129050d9c0ac6e46c9fa1b054ba4f7b4e470973
Josh Gorin <jgorin@conncoll.edu>
2023-10-17 20:09:56 -0400

    reconfigured stack

---
+++ src/diffusion_kinetics/optimization/optimization_routines.py
@@ -0,0 +1,80 @@
+import random
+from scipy.optimize import NonlinearConstraint
+from scipy.optimize import differential_evolution
+from utils.generate_bounds import generate_bounds
+from torch import torch
+from diffusion_kinetics.optimization.dataset import Dataset
+from diffusion_kinetics.optimization.diffusion_objective import DiffusionObjective
+import numpy as np
+from diffusion_kinetics.optimization.conHe_Param import conHe_Param
+
+
+def diffEV_multiples(
+    objective,
+    dataset,
+    num_iters: int,
+    ndom,
+    Ea_bounds: tuple,
+    lnd0aa_bounds: tuple,
+    max_iters: int = 30000,
+):
+    """
+    Run the differential evolution algorithm multiple times and returns the best result.
+
+    Args:
+        objective (DiffusionObjective): the objective function for the optimization problem.
+        dataset (Dataset): the dataset for the optimization problem.
+        num_iters (int): the number of times to run the differential evolution algorithm.
+        ndom (int): the number of domains in the optimization problem.
+        Ea_bounds (tuple): the bounds for the activation energy.
+        lnd0aa_bounds (tuple): the bounds for the ln(D0/a^2).
+        max_iters (int, optional): the maximum number of iterations for the differential evolution algorithm. Defaults to 30000.
+
+    Returns:
+        _type_: _description_
+    """
+    # If the number of domains > 1, enforce constraing that 1-sum(fracs) > 0. Else, no constraint.
+    if ndom > 1:
+        nlc = NonlinearConstraint(conHe_Param, lb=[0], ub=[np.inf])
+    else:
+        nlc = []
+
+    misfits = []
+    params = []
+    seed = random.randint(0, 2 ^ 28)
+    mole_bound = tuple(
+        (
+            sum(dataset.M) - 1 * torch.sqrt(sum(torch.tensor(dataset.delM) ** 2)),
+            sum(dataset.M) + 1 * torch.sqrt(sum(torch.tensor(dataset.delM) ** 2)),
+        )
+    )
+    bounds = generate_bounds(
+        ndom,
+        mole_bound,
+        objective.stat,
+        Ea_bounds=Ea_bounds,
+        lnd0aa_bounds=lnd0aa_bounds,
+    )
+
+    for i in range(num_iters):
+        result = differential_evolution(
+            objective,
+            bounds,
+            disp=False,
+            tol=0.0001,  # zeros seems like a good number from testing. slow, but useful.
+            maxiter=max_iters,
+            constraints=nlc,
+            vectorized=True,
+            updating="deferred",
+            seed=seed,
+        )
+
+        misfits.append(result.fun)
+        print(f"misfit: {result.fun}")
+        print(f"number of iterations: {result.nit}")
+        params.append(result.x)
+
+        seed += 1
+
+    index = np.argmin(misfits)
+    return params[index], misfits[index]
---
+++ src/diffusion_kinetics/utils/save_results.py
@@ -0,0 +1,35 @@
+import os
+import datetime as datetime
+import numpy as np
+
+
+def save_results(
+    sample_name: str = "", misfit_stat: str = "", params=[], moves_type=""
+):
+    """
+    Save the resulting parameters for an optimization run to a CSV.
+
+    Args:
+        sample_name (str, optional): The name of the sample. Defaults to "".
+        misfit_stat (str, optional): The type of misfit statistic used in the optimization. Defaults to "".
+        params (list, optional): The parameters from the optimization. Defaults to [].
+        moves_type (str, optional): The type of moves used in the optimization. Defaults to "".
+    """
+    # Get the current date and time
+
+    run_name = f"{misfit_stat}"
+
+    folder_name = os.path.join("results", f"{sample_name}", run_name)
+
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+    # Generate the file name
+
+    name = os.path.join(folder_name, f"output_kinetics.csv")
+
+    with open(name, "w", newline="") as file:
+        # writer = csv.writer(file)
+        # writer.writerow(np.append(params,misfit))
+        np.savetxt(file, params, delimiter=",")
+    # Save the figure as a PDF file
