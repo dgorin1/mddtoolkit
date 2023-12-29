c67cfee16f61adbfa271e4e3c6123871b31a4495
Josh Gorin <jgorin@conncoll.edu>
2023-12-29 11:34:43 -0500

    savestate

--- src/diffusion_kinetics/cli.py
+++ src/diffusion_kinetics/cli.py
@@ -10,7 +10,7 @@ def parse_args():
 
 def main():
     args = parse_args()
-    MultiPipeline(args.input, args.output).run()
+    MultiPipeline(args.input, args.output).run(args.config)
     
 if __name__ == "__main__":
     main()
