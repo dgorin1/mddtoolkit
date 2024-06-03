corrected inconesquential error in spherical Daa uncertainty calculation for the measured values. Does not pertain to anything submitted in Gorin et al. 2024.

src/diffusion_kinetics/preprocessing/calculate_experimental_results.py
@@ -82,7 +82,7 @@ def D0calc_MonteCarloErrors(expdata,geometry:str):
                 
                 # Equation XXXX from Ginster (2018)
                 uncert_a[i] = 1/(3*(cumtsec[i]-cumtsec[i-1])*np.sum(M))*np.sqrt(
-                        ((1-Fi[i])*((1/(np.sqrt(1-(math.pi/3)*Fi[i])))-1) + (1-Fi[i-1])*(1-(1/(np.sqrt(1-(math.pi/3)*Fi[i-1])))))**2*np.sum(delM[0:-1]**2) + 
+                        ((1-Fi[i])*((1/(np.sqrt(1-(math.pi/3)*Fi[i])))-1) + (1-Fi[i-1])*(1-(1/(np.sqrt(1-(math.pi/3)*Fi[i-1])))))**2*np.sum(delM[0:i-1]**2) + 
                         ((1-Fi[i])*(1/np.sqrt(1-(math.pi/3)*Fi[i])-1) - Fi[i-1]*(1 - 1/np.sqrt(1-(math.pi/3)*Fi[i-1])))**2 * delM[i]**2 + 
                         (Fi[i]* (1- (1/np.sqrt(1-(math.pi/3)*Fi[i]))) - Fi[i-1]*(1 - 1/(np.sqrt(1-(math.pi/3)*Fi[i-1])))) **2 * np.sum(delM[i+1:]**2)
                 )